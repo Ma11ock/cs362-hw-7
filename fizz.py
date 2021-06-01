61c7c602f553a9d9a506c5ae35c718e014749046
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-05-31 20:09:22 -0700

    Buzz function, fix modulo order bug.

--- fizz.py
+++ fizz.py
@@ -2,8 +2,12 @@
 # FizzBuzz.
 
 def isFizz(arg: int) -> bool:
-    if 3 % arg == 0:
+    if arg % 3 == 0:
         return True
     return False
 
 
+def isBuzz(arg: int) -> bool:
+    if arg % 5 == 0:
+        return True
+    return False
