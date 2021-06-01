2cfa378cde0cadd884eb0d55fc115559c08d1c79
Ryan Jeffrey <ryan@ryanmj.xyz>
2021-05-31 20:09:12 -0700

    Buzz tests, fizzbuzz tests.

--- fizz-test.py
+++ fizz-test.py
@@ -13,6 +13,21 @@ class TestStringMethods(unittest.TestCase):
     def test_fizz2(self):
         self.assertFalse(isFizz(2))
 
+    def test_buzz1(self):
+        self.assertTrue(isBuzz(5))
+    def test_buzz2(self):
+        self.assertTrue(isBuzz(25))
+    def test_buzz3(self):
+        self.assertFalse(isBuzz(2))
+    def test_buzz4(self):
+        self.assertFalse(isBuzz(101))
+
+    def test_fizzbuzz1(self):
+        self.assertTrue(isFizz(15) and isBuzz(15))
+    def test_fizzbuzz2(self):
+        self.assertTrue(isFizz(30) and isBuzz(30))
+    def test_fizzBuzz3(self):
+        self.assertFalse(isFizz(31) and isBuzz(31))
 
 if __name__ == '__main__':
     unittest.main()
